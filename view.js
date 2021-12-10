ecdb575ebc21737a0b850b02cf8629b1cec02ce6
Insaf <v.insaf228@gmail.com>
2021-12-10 13:47:34 +0200

    dom elements

---
+++ view.js
@@ -0,0 +1,6 @@
+export const typeHighTask = document.querySelector('.type_high_task');
+export const typeLowTask = document.querySelector('.type_low_task');
+export const highPriority = document.querySelector('.high_priority');
+export const lowPriority = document.querySelector('.low_priority');
+export const textHighInput = document.querySelector('.plus_high_button');
+export const textLowInput = document.querySelector('.plus_low_button');
